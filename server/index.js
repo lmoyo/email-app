ee852d4980241d1c7ebeabf200d1c612ec4d6dd8
Lo <lo@Los-MacBook-Pro-3.local>
2020-11-23 15:12:17 -0500

    added mongo

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 //https://polar-escarpment-92250.herokuapp.com/
 
 const express = require('express');
+const mongoose = require('mongoose')
 const passportConfig = require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
+
 
 const app = express();
 
 authRoutes(app);
-
 
 
 const PORT = process.env.PORT || 5000; //injected from heroku
